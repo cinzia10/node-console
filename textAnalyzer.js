3beced5a18dcd86af5862cb1d5bf9ff7d8b6adaa
LittleAkira <cinzia.ariotti.10@gmail.com>
2022-05-12 12:32:11 +0200

    update

---
+++ textAnalyzer.js
@@ -0,0 +1,55 @@
+/////// STEP 1 - IMPORTARE FS
+
+const fs = require('fs');
+const myArgs = process.argv.slice(2);
+
+/////// STEP 2 - LEGGERE GLI ARGOMENTI INSERITI DA CONSOLE
+
+if (myArgs[0] === undefined) {
+  console.error('mi serve il file di input')
+  process.exit();
+}
+
+const inputUrl = myArgs[0];
+
+let outputUrl
+
+if (myArgs[1] === undefined){
+  outputUrl = inputUrl;
+} else {
+  outputUrl = myArgs[1];
+}
+
+let data;
+
+try {
+  data = fs.readFileSync(inputUrl, 'utf8');
+} catch (err) {
+  console.log('file non trovato');
+  process.exit();
+}
+
+/////// STEP 3 - PRENDERE IL PRIMO ELEMENTO COME "INPUTURL" E IL SECONDO COME "OUTPUTURL" (NON OBBLIGATORIO)
+
+
+/////// STEP 4 - LEGGERE IL CONTENUTO DEL FILE E LOGGARE IL NUMERO DI CARATTERI (SPAZI COMPRESI) E IL NUMERO DI CARATTERI (SPAZI ESCLUSI)
+
+const inputLenghtWithSpaces = data.length; 
+console.log(inputLenghtWithSpaces); 
+
+const inputLenghtWithoutSpaces = data.replace(/\s+/g, '').length;
+console.log(inputLenghtWithoutSpaces);
+
+
+const result = data + '\r\n' + '\r\n' + 'Numero caratteri (spazi compresi): ' + inputLenghtWithSpaces + '\r\n' + '\r\n' + 'Numero caratteri (spazi esclusi): ' + + inputLenghtWithoutSpaces;
+
+try {
+  fs.writeFileSync(outputUrl, result);
+} catch (err) {
+  console.error('non riesco a scrivere il file');
+  process.exit();
+}
+
+/////// STEP 5 - SE L'UTENTE HA INSERITO L'"OUTPUTURL", SCRIVERE UN NUOVO FILE CON IL TESTO DELL'ORIGINALE PIU' I DATI DELL'ANALISI
+///////          SE L'UTENTE NON HA INSERITO L'"OUTPUTURL", SOVRASCRIVERE IL FILE ORIGINALE.
+/////// ESEMPIO: (ORIGINALE)"VIVA IL CSS!" (COPIA)"VIVA IL CSS!" \ NUMERO DI CARATTERI (SPAZI INCLUSI): 12 \ NUMERO DI CARATTERI (SPAZI ESCLUSI): 10

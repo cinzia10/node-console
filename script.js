09948774f13658e33966242e136c0a541069079e
LittleAkira <cinzia.ariotti.10@gmail.com>
2022-05-12 11:24:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ const fs = require('fs');
 let data;
 
 try {
-    data = fs.readFileSync('./libri.csv', 'utf8');
+    data = fs.readFileSync(inputUrl, 'utf8');
   } catch (err) {
     console.error('file non trovato');
   }
@@ -96,3 +96,33 @@ try {
     } catch {
       console.error('file non trovato');
     }
+
+
+const myArgs = process.argv.slice(2);
+
+if (myArgs[0] === undefined){
+    console.log("mi serve il file di input");
+    process.exit();
+}
+
+const inputUrl = myArgs[0];
+
+let outputUrl
+
+if (myArgs[1] === undefined){
+    outputUrl = './output.json';
+} else {
+    outputUrl = myArgs[1];
+}
+
+
+
+// checkType(value){
+//     if (! isNaN(value)) {
+//         return parseFloat(value);
+//     } else if (value==="true"||value==="false") {
+//         return value==="true"?true:false;
+//     } else {
+//         return value;
+//     }
+// };
